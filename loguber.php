3e807802b892d39e5db3d188fcb573605ceb01d4
Ebina <mxea@gft.com>
2018-03-23 16:04:28 -0300

    cariando página loguber

--- loguber.php
+++ loguber.php
@@ -6,9 +6,59 @@
     			<h1>LOGUBER</h1>
     		</div>
     		<section>
-    			<p>
-                    Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber Loguber.
-                </p>
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="baseboard loguber text">
+                            <h2>TRANSPORTANDO PARA TODOS</h2>
+                            <h3>Uma empresa constituída para atender uma nova forma de gestão em transportes de cargas no mercado.</h3>
+                            <p>
+                                É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. 
+                            </p>
+                            <div class="item">
+                                <span class="left-t hidden-sm hidden-md hidden-lg"></span>
+                                <span class="right-t"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="baseboard loguber image">
+                            <div class="item">
+                                <span class="left-t hidden-sm hidden-md hidden-lg"></span>
+                                <span class="right-t"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
     		</section>
+            <div class="blue-strip">
+                <section>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="white-panel">
+                                <h4>Missão</h4>
+                                <p>
+                                    Missão missão missão missão missão missão missão missão missão missão missão missão missão missão missão missão.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="white-panel">
+                                <h4>Visão</h4>
+                                <p>
+                                    Visão visão visão visão visão visão visão visão visão visão visão visão visão visão visão visão visão visão visão.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="white-panel">
+                                <h4>Valores</h4>
+                                <p>
+                                    Valores valores valores valores valores valores valores valores valores valores valores valores valores valores valores.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
 	    </div>
 	<?php include("footer.php"); ?>
