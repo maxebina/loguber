e4ddcbc7380702726211af3cdffe8c6a75a0b702
Max Ebina <max@maxebina.com.br>
2018-04-22 19:16:19 -0300

    pagina franquia

--- footer.php
+++ footer.php
@@ -23,6 +23,24 @@
         <script src="js/validator.js"></script>
         <script src="js/contact.js"></script>
         <script src="js/change-value.js"></script>
+        <script type="text/javascript">
+            $(function() {
+                $('#login-form-link').click(function(e) {
+                    $("#login-form").delay(100).fadeIn(100);
+                    $("#register-form").fadeOut(100);
+                    $('#register-form-link').removeClass('active');
+                    $(this).addClass('active');
+                    e.preventDefault();
+                });
+                $('#register-form-link').click(function(e) {
+                    $("#register-form").delay(100).fadeIn(100);
+                    $("#login-form").fadeOut(100);
+                    $('#login-form-link').removeClass('active');
+                    $(this).addClass('active');
+                    e.preventDefault();
+                });
+            });
+        </script>
     </div>
 </body>
 </html>
--- franquia.php
+++ franquia.php
@@ -2,13 +2,47 @@
     <?php $your_meta_description="Descrição aqui"; ?>
 	<?php include("header.php"); ?>
 		<div class="page">
-    		<div class="banner-page banner-loguber">
+    		<div class="banner-page banner-franquia">
     			<h1>FRANQUIA</h1>
     		</div>
     		<section>
-    			<p>
-    				Franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia franquia.
-                </p>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-login">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-6">
+                                        <a href="#" class="active" id="login-form-link">
+                                            <h2>FRANQUIA DO MOTORISTA</h2>
+                                            <h3>O QUE É O MODELO DE FRANQUIA PARA OS MOTORISTAS?</h3>
+                                        </a>
+                                    </div>
+                                    <div class="col-xs-6">
+                                        <a href="#" id="register-form-link">
+                                            <h2>FRANQUIA DO VENDEDOR</h2>
+                                            <h3>O QUE É O MODELO DE FRANQUIA PARA OS VENDEDORES?</h3>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>                
     		</section>
+            <section>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div id="login-form">
+                                Login
+                            </div>
+                            <div id="register-form">
+                                Register
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 	    </div>
 	<?php include("footer.php"); ?>
