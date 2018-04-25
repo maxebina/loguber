6527018446088b944bf037a78d511479aeda94dd
Ebina <mxea@gft.com>
2018-04-25 14:23:53 -0300

    pagina notícias

--- noticias.php
+++ noticias.php
@@ -1,14 +1,33 @@
 	<?php $thisPage="Notícias"; ?>
     <?php $your_meta_description="Descrição aqui"; ?>
 	<?php include("header.php"); ?>
-		<div class="page">
+		<div class="page noticias">
     		<div class="banner-page banner-loguber">
     			<h1>NOTÍCIAS</h1>
     		</div>
     		<section>
-    			<p>
-    				Notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias notícias.
-                </p>
+                <div class="row">
+                    <div class="col-md-7">
+                        <a href="por-que-saber-o-minimo-de-logistica">
+                            <img src="img/por-que-saber-o-minimo-de-logistica-01.jpg" class="noticia-img">
+                        </a>
+                    </div>
+                    <div class="col-md-5">
+                        <h2>
+                            POR QUE SABER O MÍNIMO DE LOGÍSTICA?
+                        </h2>
+                        <h3 class="mb-20">
+                            Essa é uma bela pergunta para qualquer empresário. Você vende seus produtos, cota o frete e despacha para o cliente. E fim, certo? Na verdade, muitas vezes esse final não é tão feliz assim. A culpa...
+                        </h3>
+                        <a href="por-que-saber-o-minimo-de-logistica" class="btn btn-primary btn-lg fr" role="button">
+                            Leia mais 
+                            <i class="fa fa-arrow-right"></i>
+                        </a>
+                        <div class="data-noticia">
+                            <i class="fa fa-calendar"></i> Publicado no dia 01/05/2018
+                        </div>
+                    </div>
+                </div>    			
     		</section>
 	    </div>
 	<?php include("footer.php"); ?>
---
+++ por-que-saber-o-minimo-de-logistica.php
@@ -0,0 +1,85 @@
+	<?php $thisPage="Notícias"; ?>
+    <?php $your_meta_description="Descrição aqui"; ?>
+	<?php include("header.php"); ?>
+		<div class="page noticias">
+    		<div class="banner-page banner-loguber">
+    			<h1>NOTÍCIAS</h1>
+    		</div>
+    		<section>
+                <h2>
+                    POR QUE SABER O MÍNIMO DE LOGÍSTICA?
+                </h2>
+                <div class="data-noticia interno mb-20">
+                    <i class="fa fa-calendar"></i> Publicado no dia 01/05/2018
+                </div>
+                <img src="img/por-que-saber-o-minimo-de-logistica-01.jpg" class="noticia-interno-img">
+                <p>
+                    Essa é uma bela pergunta para qualquer empresário. Você vende seus produtos, cota o frete e despacha para o cliente. E fim, certo? Na verdade, muitas vezes esse final não é tão feliz assim. A culpa, de quem é, não importa. Seu cliente acaba não recebendo a mercadoria, ou recebendo-a com atrasos, e quem paga o pato é você. Talvez se você entendesse um pouquinho mais sobre a logística de seus produtos, problemas desse tipo poderiam ser evitados.
+                </p>
+                <p>
+                    Entender os processos que ocorrem entre o momento da venda e o recebimento do produto pelo cliente é algo de essencial para seu negócio. Às vezes, um problema detectado com horas de antecedência pode ser facilmente resolvido com medidas como o reenvio, um contato direto junto ao cliente, ou uma solicitação de retorno junto à transportadora. Compreender o processo a fundo, ainda que você o esteja terceirizando, é uma prática que pode evitar problemas como:
+                </p>
+                <ul>
+                    <li>
+                        Atrasos no recebimento por parte do cliente;
+                    </li>
+                    <li>
+                        Falhas e erros na documentação.
+                    </li>
+                    <li>
+                        Problemas de embarque e desembarque, ou dificuldades que possam estragar seu planejamento.
+                    </li>
+                    <li>
+                        Problemas de embarque e desembarque, ou dificuldades que possam estragar seu planejamento.
+                    </li>
+                    <li>
+                        Posições de estoque e inventários.
+                    </li>
+                    <li>
+                        Rotas de maior dificuldade ou com problemas.
+                    </li>
+                    <li>
+                        Desperdícios e danos a produtos e remessas.
+                    </li>
+                    <li>
+                        Cobranças de extras ou indevidas.
+                    </li>
+                    <li>
+                        Cobranças de impostos por atraso no recolhimento ou erro no preenchimento de guias e documentos.
+                    </li>
+                </ul>
+                <p>
+                    Conhecer o processo e a logística não apenas pode tornar seu negócio mais eficiente, mas também pode reduzir seus custos. Erros, desperdícios, atrasos e problemas na entrega, muitas vezes, são fatores que tornam a margem de lucro em determinado produto praticamente inexistente.
+                </p>
+                <p>
+                    <strong>QUE INFORMAÇÕES PROCURAR?</strong>
+                </p>
+                <p>
+                    Não é preciso que você se torne um “expert” em logística, mas é necessário que você esteja sempre em linha com informações da transportadora e clientes, e alguns conceitos e procedimentos devem ser de seu total conhecimento, entre eles:
+                </p>
+                <ul>
+                    <li>
+                        Forma e método de acomodação da carga fracionada e volumes em caminhões e meios de transporte.
+                    </li>
+                    <li>
+                        Manuseio de produtos em armazéns e terminais.
+                    </li>
+                    <li>
+                        Embalo e detalhes técnicos, como o coeficiente de empilhamento.
+                    </li>
+                    <li>
+                        Cubagem e cálculos de custos e remuneração a transportadoras.
+                    </li>
+                    <li>
+                        Documentação e emissão de guias.
+                    </li>
+                    <li>
+                        Detalhes contábeis referentes à estocagem e movimentação de produtos.
+                    </li>
+                </ul>
+                <p>
+                    Parece coisa demais, mas tudo em prol da saúde de seu negócio. Seu cliente agradece, a transportadora agradece, mas principalmente: seu bolso agradece.
+                </p>
+    		</section>
+	    </div>
+	<?php include("footer.php"); ?>
