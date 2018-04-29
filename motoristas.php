8f4417c883864c5360c1640090dee1d1b89fb521
Max Ebina <max@maxebina.com.br>
2018-04-28 21:58:39 -0300

    pagina motoristas

--- motoristas.php
+++ motoristas.php
@@ -2,13 +2,63 @@
     <?php $your_meta_description="Descrição aqui"; ?>
 	<?php include("header.php"); ?>
 		<div class="page">
-    		<div class="banner-page banner-loguber">
+    		<div class="banner-page banner-motoristas">
     			<h1>MOTORISTAS</h1>
     		</div>
     		<section>
-    			<p>
-    				Motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas motoristas.
-                </p>
-    		</section>
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="baseboard loguber image-motoristas">
+                            <div class="item">
+                                <span class="left-t hidden-sm hidden-md hidden-lg"></span>
+                                <span class="right-t"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="baseboard loguber-motoristas text">
+                            <h2>SUB TÍTULO MOTORISTAS</h2>
+                            <h3>Algum texto sobre motoristas</h3>
+                            <p>
+                                Profissionais com mais de 30 anos de experiência em transportes e gestão de recursos humanos.
+                            </p>
+                            <div class="item">
+                                <span class="left-t hidden-sm hidden-md hidden-lg"></span>
+                                <span class="right-t"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div class="blue-strip">
+                <section>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="white-panel">
+                                <h4>Serviços</h4>
+                                <p>
+                                    Saiba mais sobre serviços prestados pela <strong>LOGUBER</strong>
+                                </p>
+                                <a href="servicos" class="btn btn-primary mb-20" role="button">
+                                    Leia mais 
+                                    <i class="fa fa-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="white-panel">
+                                <h4>Franquia</h4>
+                                <p>
+                                    Entenda mais sobre as modalidades de franquia da <strong>LOGUBER</strong>
+                                </p>
+                                <a href="franquia" class="btn btn-primary mb-20" role="button">
+                                    Leia mais 
+                                    <i class="fa fa-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
 	    </div>
 	<?php include("footer.php"); ?>
