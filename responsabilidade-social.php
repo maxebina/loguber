00182cbf6fe1c0e00795fe34451cb8b2d8db00b8
Max Ebina <max@maxebina.com.br>
2018-05-02 21:17:53 -0300

    ajustes de página

--- responsabilidade-social.php
+++ responsabilidade-social.php
@@ -8,44 +8,56 @@
             <div class="dark-blue-strip">
                 <i class="fa fa-globe"></i>
                 <h2>
-                    COMPROMISSO COM O DESENVOLVIMENTO SUSTENTÁVEL DA SOCIEDADE
+                    APOIO INSTITUCIONAL E COMPROMETIMENTO COM A SOCIEDADE
                 </h2>
                 <h3>
-                    Projetos sociais apoiados pela LOGUBER
+                    Vôcê também pode apoiar essas causas
                 </h3>
             </div>
     		<section>
                 <div class="row">
                     <div class="col-md-4">
                         <div class="panel panel-shadow">
-                            <img src="img/logo-rs-ca.png" title="Campanha do Agasalho">
+                            <a href="http://www.campanhadoagasalho.sp.gov.br/" target="_blank">
+                                <img src="img/logo-rs-ca.png" title="Campanha do Agasalho">
+                            </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="panel panel-shadow">
-                            <img src="img/logo-rs-ma.png" title="Maio Amarelo">
+                            <a href="https://www.maioamarelo.com/" target="_blank">
+                                <img src="img/logo-rs-ma.png" title="Maio Amarelo">
+                            </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="panel panel-shadow">
-                            <img src="img/logo-rs-mc.png" title="Na Mão Certa">
+                            <a href="http://www.namaocerta.org.br/" target="_blank">
+                                <img src="img/logo-rs-mc.png" title="Na Mão Certa">
+                            </a>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4">
                         <div class="panel panel-shadow">
-                            <img src="img/logo-rs-na.png" title="Novembro Azul">
+                            <a href="http://www.ladoaladopelavida.org.br/novembro-azul" target="_blank">
+                                <img src="img/logo-rs-na.png" title="Novembro Azul">
+                            </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="panel panel-shadow">
-                            <img src="img/logo-rs-or.png" title="Outubro Rosa">
+                            <a href="http://www.fundacaolacorosa.com/" target="_blank">
+                                <img src="img/logo-rs-or.png" title="Outubro Rosa">
+                            </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="panel panel-shadow">
-                            <img src="img/logo-rs-sv.png" title="Setembro vermelho">
+                            <a href="https://www.ladoaladopelavida.org.br/setembro-vermelho" target="_blank">
+                                <img src="img/logo-rs-sv.png" title="Setembro vermelho">
+                            </a>
                         </div>
                     </div>
                 </div>
