0bca4d907b97ae8325d5e0786f8eadef4f170b84
Max Ebina <max@maxebina.com.br>
2018-05-09 19:52:55 -0300

    ajustes página motoristas

--- motoristas.php
+++ motoristas.php
@@ -1,64 +1,154 @@
 	<?php $thisPage="Motoristas"; ?>
-    <?php $your_meta_description="Descrição aqui"; ?>
+  <?php $your_meta_description="Descrição aqui"; ?>
 	<?php include("header.php"); ?>
-		<div class="page">
-    		<div class="banner-page banner-motoristas">
-    			<h1>MOTORISTAS</h1>
-    		</div>
-    		<section>
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="baseboard loguber image-motoristas">
-                            <div class="item">
-                                <span class="left-t hidden-sm hidden-md hidden-lg"></span>
-                                <span class="right-t"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="baseboard loguber-motoristas text">
-                            <h2>SUB TÍTULO MOTORISTAS</h2>
-                            <h3>Algum texto sobre motoristas</h3>
-                            <p>
-                                Profissionais com mais de 30 anos de experiência em transportes e gestão de recursos humanos.
-                            </p>
-                            <div class="item">
-                                <span class="left-t hidden-sm hidden-md hidden-lg"></span>
-                                <span class="right-t"></span>
-                            </div>
-                        </div>
-                    </div>
+	<div class="page">
+		<div class="banner-page banner-motoristas">
+			<h1>TECNOLOGIA</h1>
+		</div>
+		<section>
+      <h2 class="mt-40 mb-20">PRECISA COLOCAR UM TÍTULO SOBRE MOTORISTAS AQUI</h2>
+      <div class="row">
+        <div class="col-md-7">
+          <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingOne">
+                <h3 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne" value="Click">
+                    INCLUSÃO DAS MULHERES NO TRANSPORTES DE CARGAS:
+                  </a>
+                </h3>
+
+              </div>
+              <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                  <p>
+                    Temos a preocupação e compromisso de inserir mulheres na direção de nossos caminhões, visando à igualdade e compromisso de um bom atendimento
+                  </p>
                 </div>
-            </section>
-            <div class="blue-strip">
-                <section>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="white-panel">
-                                <h4>Serviços</h4>
-                                <p>
-                                    Saiba mais sobre serviços prestados pela <strong>LOGUBER</strong>
-                                </p>
-                                <a href="servicos" class="btn btn-primary mb-20" role="button">
-                                    Leia mais 
-                                    <i class="fa fa-arrow-right"></i>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="white-panel">
-                                <h4>Franquia</h4>
-                                <p>
-                                    Entenda mais sobre as modalidades de franquia da <strong>LOGUBER</strong>
-                                </p>
-                                <a href="franquia" class="btn btn-primary mb-20" role="button">
-                                    Leia mais 
-                                    <i class="fa fa-arrow-right"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+              </div>
             </div>
-	    </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingTwo">
+                <h3 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" value="Click">
+                    POLÍTICAS DE SEGURANÇA, SAÚDE E MEIO AMBIENTE
+                  </a>
+                </h3>
+
+              </div>
+              <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="panel-body">
+                  <p>
+                    As políticas de segurança, tanto as da profissão quanto as estabelecidas pela empresa, devem ser exigidas de um motorista para permitir o engajamento dele de acordo com os padrões da organização para reduzir o risco de perdas. Essa capacitação foca no treinamento para a prevenção de acidentes que possam gerar danos à empresa, à vida e ao meio ambiente. 
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingThree">
+                <h3 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" value="Click">
+                    CURSO DE DIREÇÃO DEFENSIVA
+                  </a>
+                </h3>
+
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <p>
+                    O curso é voltado para os motoristas de transporte de passageiros e carga que visa um treinamento para a mudança dos hábitos inadequados ao volante. A capacitação inclui a parte teórica e aulas práticas necessárias ao aprendizado. O motorista passa por avaliações com relação à aplicação dos conceitos de direção defensivapara eliminar qualquer tipo de “vício” que possa ter ao volante e por em risco a segurança do transporte. 
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingFour">
+                <h3 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour" value="Click">
+                    GERENCIAMENTO DE RISCOS
+                  </a>
+                </h3>
+
+              </div>
+              <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+                <div class="panel-body">
+                  <p>
+                    A capacitação em gerenciamento de riscos não deve ser uma exigência exclusiva da equipe de gestão de frotas. É um curso que o motorista vai precisar ter para conhecer e controlar os riscos que envolvam uma operação de transporte de cargas e passageiros da empresa. Dessa forma ele poderá atuar também como um especialista na segurança do trabalho.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingFive">
+                <h3 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive" value="Click">
+                    CURSO MOPP
+                  </a>
+                </h3>
+
+              </div>
+              <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+                <div class="panel-body">
+                  <p>
+                    Em parceria com o DETRAN, o curso MOPP é oferecido a motoristas que desejam obter uma certificação de transporte seguro para cargas e passageiros. O curso e treinamento são muito valiosos para o motorista e importante para a empresa contratante ter a certeza de integrar à equipe um profissional competente. 
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingSix">
+                <h3 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix" value="Click">
+                    CURSO SOBRE A RESOLUÇÃO 168
+                  </a>
+                </h3>
+
+              </div>
+              <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+                <div class="panel-body">
+                  <p>
+                    O curso sobre a resolução 168 é indispensável para o motorista que queira tornar-se um profissional competitivo no mercado de trabalho. Ele visa a reciclagem do profissional quanto as normas e procedimentos vigentes com relação ao transporte de cargas e passageiros de forma segura e confortável. 
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingSeven">
+                <h3 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven" value="Click">
+                    ÉTICA PESSOAL E PROFISSIONAL
+                  </a>
+                </h3>
+
+              </div>
+              <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+                <div class="panel-body">
+                  <p>
+                    A conduta de um profissional pode ser medida de acordo com os comportamentos apresentados de forma individual, ou na coletividade, e é uma das maiores preocupações das empresas de transporte atualmente. O motorista deve ser capacitado para saber se portar diante de situações que exijam dele a confiança necessária que a empresa precisa. A capacitação em ética pessoal e profissional pode ajudar a reduzir os custos com roubos e furtos e melhorar a produtividade da empresa. 
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div class="col-md-5">
+          <img src="img/featured-caminhao-motoristas.png" class="img-motoristas" />
+          <div class="baseboard">
+              <div class="item">
+                <span class="left-t hidden-sm hidden-md hidden-lg"></span>
+                <span class="right-t"></span>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
 	<?php include("footer.php"); ?>
