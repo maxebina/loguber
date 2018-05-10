a59da29917256aef8e1f3e88bde008e9155762ea
Ebina <mxea@gft.com>
2018-05-10 17:01:23 -0300

    analytics

--- contato.php
+++ contato.php
@@ -72,7 +72,7 @@
     		</section>
         </div>
         <div class="row">
-            <div class="col-md-6">                
+            <div class="col-md-6">
                 <div class="map-box">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.198246070766!2d-46.712026084478886!3d-23.525371084700744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef8883d4dc039%3A0xde849eed513b3932!2sR.+Bar%C3%A3o+de+Jundia%C3%AD%2C+523+-+Lapa%2C+S%C3%A3o+Paulo+-+SP%2C+05073-010!5e0!3m2!1spt-BR!2sbr!4v1524310767726" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen"></iframe>
                 </div>
@@ -95,6 +95,9 @@
                     </p>
                     <p>
                         comercial@loguber.com.br
+                    </p>
+                    <p>
+                        Em breve mais uma filial em São Paulo.
                     </p>
                 </div>
             </div>
