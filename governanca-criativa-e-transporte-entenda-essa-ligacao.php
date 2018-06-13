7d28de547b290313690d7a957cb3ce2c4e4feb3a
Ebina <mxea@gft.com>
2018-06-13 16:46:53 -0300

    nova notícia

---
+++ governanca-criativa-e-transporte-entenda-essa-ligacao.php
@@ -0,0 +1,107 @@
+	<?php $thisPage="Notícias"; ?>
+    <?php $your_meta_description="Descrição aqui"; ?>
+	<?php include("header.php"); ?>
+		<div class="page noticias">
+    		<div class="banner-page banner-noticias">
+    			<h1>NOTÍCIAS</h1>
+    		</div>
+    		<section>
+                <h2>
+                    Governança criativa e transporte: entenda essa ligação
+                </h2>
+                <div class="data-noticia interno mb-20">
+                    <i class="fa fa-calendar"></i> Publicado no dia 13/06/2018
+                </div>
+
+                <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-506da23b78eb0a51"></script>
+                <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                <div class="addthis_inline_share_toolbox"></div>
+            
+                <img src="img/governanca-criativa-e-transporte-entenda-essa-ligacao.jpg" class="noticia-interno-img">
+                <p>
+                    A sociedade moderna conta não apenas com tecnologia avançada, mas também com novas estratégias, resultantes das recentes pesquisas e estudos nas diferentes áreas do saber. Para auxiliar a gestão de uma empresa, existem sistemas automatizados, como ERP e WMS, e estratégias como a governança criativa.
+                </p>
+                <p>
+                    O nome, por si só, já é estimulante. Essa estratégia pode ser aplicada em todas as áreas da gestão corporativa, inclusive nosetor logístico e, mais especificamente, no transporte de cargas.
+                </p>
+                <p>
+                    Saiba mais sobre essa ferramenta de gestão e como ela pode dar novos rumos à sua empresa!
+                </p>
+                <h3>
+                    O que é governança criativa
+                </h3>
+                <p>
+                    O conceito reúne criatividade e tecnologia para administrar o negócio, gerando um diferencial competitivo no mercado. A finalidade é, naturalmente, produzir mais vendas, conquistar mais clientes e cativar novos parceiros.
+                </p>
+                <p>
+                    A governança criativa está associada e se identifica com outros conceitos, como governança corporativa e economia criativa. Trata-se de uma tendência que se impõe cada vez mais no mercado atual.
+                </p>
+                <p>
+                    As empresas modernas procuram ofertar experiências melhores aos clientes, focando no atendimento diferenciado e no valor agregado aos serviços prestados. A criatividade envolve todas as possibilidades originais e dinâmicas capazes de impulsionar a produtividade, agradando a todos: funcionários, clientes e parceiros em geral.
+                <h3>
+                    As vantagens da criatividade
+                </h3>
+                <p>
+                    A governança criativa é responsável pela produção de conteúdos e soluções criativas que oferecem impactos, geralmente mais fortes do que os próprios apelos de vendas. A criatividade apresenta a vantagem de contextualizar as necessidades de transporte, propondo soluções mais eficientes que satisfazem os colaboradores e os clientes.
+                </p>
+                <p>
+                    Uma gestão criativa não tem medo de inovar e de ser original, desde que essa originalidade ofereça resultados concretos, que contribuam para reduzir custos com transporte sem afetar a qualidade dos serviços — ao contrário, aumentando a qualidade e a produtividade no setor de transportes.
+                </p>
+                <p>
+                    O transporte continua sendo a etapa mais dispendiosa da cadeialogística e de suprimentos, do ponto de vista financeiro e de tempo. Logo, encontrar soluções para reduzir esses gastos é uma necessidade de toda empresa, embora nem sempre seja fácil de ser atendida.
+                </p>
+                <p>
+                    O gestor criativo está aberto a novas soluções (especialmente no que se refere a ferramentas tecnológicas), a novos empreendimentos e a tudo que seus colaboradores sugerem e descobrem. A criatividade na gestão também é isso: o compartilhamento de ideias e a tomada de decisões em conjunto (a palavra final é do gestor, mas as opiniões dos outros também são consideradas).
+                </p>
+                <h3>
+                    A tecnologia como aliada para solucionar problemas
+                </h3>
+                <p>
+                    Esse é um dos primeiros pontos a considerar quando o assunto é governança criativa. Falar em criatividade tem muita relação com falar emtecnologia, pois esta nada mais é que o resultado do desenvolvimento intelectual e criativo da sociedade.
+                </p>
+                <p>
+                    Existem diversas maneiras de aplicar a tecnologia na gestão corporativa, utilizando não apenas o ERP, o WMS e o TMS (Sistema de Gerenciamento de Transporte), mas também aplicativos simples que podem ser instalados em aparelhos móveis (smartphones, iPhones, tablets) e que otimizam a comunicação da empresa com o motorista.
+                </p>
+                <p>
+                    Os computadores de bordo, por exemplo, permitem à empresa acompanhar o trajeto do veículo em suarota de entrega e também servem para controlar o comportamento do motorista. Acontece da mesma forma nos sistemas de rastreamento e monitoramento.
+                </p>
+                <p>
+                    Depois que a tecnologia cloud computing se consolidou, muitas empresas passaram a usar o sistema na nuvem para o armazenamento de documentos, troca de informações, gestão de dados, controle de multas, notificações e licenciamento de veículos. A nuvem pode ser acessada em qualquer lugar e de qualquer dispositivo, sem a necessidade de instalação de software — exige-se somente conexão com a internet.
+                </p>
+                <p>
+                    Existem roteirizadores que planejam rotas automáticas e eficazes, contemplando as menores distâncias, as estradas mais seguras, a existência de postos de abastecimento e hotéis.
+                </p>
+                <p>
+                    O Big Data e o Business Intelligence são outros exemplos de tecnologia que podem ser usadas pela governança criativa para dinamizar a gestão de transporte da empresa, recolhendo dados que serão transformados em importantes informações para a tomada de decisão — a qual pode afetar, direta ou indiretamente, o setor de transportes.
+                </p>
+                <p>
+                    São inúmeras as possibilidades, e cabe ao gestor criativo identificar as oportunidades e saber usá-las. Os sistemas automatizados conferem mais proteção e segurança aos cidadãos, reduzindo roubos e clonagem de diferentes tipos de veículos. Também diminuem erros que levam a multas e apreensões.
+                </p>
+                <p>
+                    Muitos problemas são solucionados pela análise e cruzamento de dados.
+                </p>
+                <p>
+                    As soluções automatizadas vêm sendo utilizadas pelas próprias instituições governamentais, como DETRAN, ANTT, VALEC, INFRAERO, Secretaria dos Portos, e outras.
+                </p>
+                <h3>
+                    O software que encontra caminhoneiros
+                </h3>
+                <p>
+                    Outra inovação criativa é omodelo moderno de transportadora, que não é contrário ao modelo tradicional, mas o amplia. O que você pensaria de um transportadora que não tem frota própria? Faz sentido para você? Pois é um modelo que existe, e é muito eficaz.
+                </p>
+                <p>
+                    Esse tipo de transportadora é, na verdade, uma intermediadora entre empresas que precisam transportar seus produtos até determinado destino e caminhoneiros autônomos que buscam prestar serviços de transporte de carga — especialmente aqueles que, após uma viagem de entrega, estão sem carga para retornar, o que é, indubitavelmente, prejudicial para as finanças e para o negócio.
+                </p>
+                <p>
+                    A empresa intermediadora, por meio do uso de tecnologia avançada (softwares que cruzam dados e rotas diversas), encontra oscaminhoneiros autônomos ideais para a empresa que precisa transportar determinada carga.
+                </p>
+                <p>
+                    Dessa forma, todas as partes saem ganhando e, geralmente, a empresa encontra um transportador a custos mais acessíveis.
+                </p>
+                <p>
+                    Esse também é um exemplo de governança criativa, já que inaugura um novo modelo de empresa especializada em transporte de carga, por meio de uma solução que une praticidade, flexibilidade e controle de gastos.
+                </p>
+    		</section>
+	    </div>
+	<?php include("footer.php"); ?>
--- noticias.php
+++ noticias.php
@@ -6,21 +6,44 @@
     		<div class="banner-page banner-noticias">
     			<h1>NOTÍCIAS</h1>
     		</div>
-    		<section class="responsabilidade">
+    		<section>
                 <div class="row">
                     <div class="col-md-7">
-                        <a href="por-que-saber-o-minimo-de-logistica">
-                            <img src="img/por-que-saber-o-minimo-de-logistica-01.jpg" class="noticia-img">
+                        <a href="governanca-criativa-e-transporte-entenda-essa-ligacao">
+                            <img src="img/governanca-criativa-e-transporte-entenda-essa-ligacao.jpg" class="noticia-img">
                         </a>
                     </div>
                     <div class="col-md-5">
                         <h2>
-                            POR QUE SABER O MÍNIMO DE LOGÍSTICA?
+                            Governança criativa e transporte: entenda essa ligação
                         </h2>
                         <h3 class="mb-20">
-                            Essa é uma bela pergunta para qualquer empresário. Você vende seus produtos, cota o frete e despacha para o cliente. E fim, certo? Na verdade, muitas vezes esse final não é tão feliz assim. A culpa...
+                            A sociedade moderna conta não apenas com tecnologia avançada, mas também com novas estratégias, resultantes das...
                         </h3>
-                        <a href="por-que-saber-o-minimo-de-logistica" class="btn btn-primary btn-lg fr" role="button">
+                        <a href="governanca-criativa-e-transporte-entenda-essa-ligacao" class="btn btn-primary btn-lg fr" role="button">
+                            Leia mais 
+                            <i class="fa fa-arrow-right"></i>
+                        </a>
+                        <div class="data-noticia">
+                            <i class="fa fa-calendar"></i> Publicado no dia 13/06/2018
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-20"></div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <a href="por-que-saber-o-minimo-de-logistica">
+                            <img src="img/por-que-saber-o-minimo-de-logistica-01.jpg" class="noticia-img">
+                        </a>
+                    </div>
+                    <div class="col-md-8">
+                        <h3>
+                            POR QUE SABER O MÍNIMO DE LOGÍSTICA?
+                        </h3>
+                        <p class="mb-20">
+                            Essa é uma bela pergunta para qualquer empresário. Você vende seus produtos, cota o frete e despacha para o cliente. E fim, certo? Na verdade, muitas vezes esse final não é tão feliz assim. A culpa...
+                        </p>
+                        <a href="por-que-saber-o-minimo-de-logistica" class="btn btn-primary fr" role="button">
                             Leia mais 
                             <i class="fa fa-arrow-right"></i>
                         </a>
@@ -28,7 +51,7 @@
                             <i class="fa fa-calendar"></i> Publicado no dia 01/05/2018
                         </div>
                     </div>
-                </div>    			
+                </div>
     		</section>
 	    </div>
 	<?php include("footer.php"); ?>
