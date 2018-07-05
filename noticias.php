e0910e06722630444b247d20f479ab6bd604dcc8
Ebina <mxea@gft.com>
2018-07-05 09:40:51 -0300

    nova notícia

--- noticias.php
+++ noticias.php
@@ -8,40 +8,61 @@
     		</div>
     		<section>
                 <div class="row">
-                    <div class="col-md-7">
-                        <a href="governanca-criativa-e-transporte-entenda-essa-ligacao">
-                            <img src="img/governanca-criativa-e-transporte-entenda-essa-ligacao.jpg" class="noticia-img">
+                    <div class="col-md-6">
+                        <a href="saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria">
+                            <img src="img/saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria-01.jpg" class="noticia-img">
                         </a>
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-6">
                         <h2>
-                            Governança criativa e transporte: entenda essa ligação
+                            Saiba quais são os diferentes tipos de carga rodoviária
                         </h2>
                         <h3 class="mb-20">
-                            A sociedade moderna conta não apenas com tecnologia avançada, mas também com novas estratégias, resultantes das...
+                            O crescimento e modernização da indústria ao redor do mundo, somado ao desenvolvimento tecnológico, que possibilita a...
                         </h3>
-                        <a href="governanca-criativa-e-transporte-entenda-essa-ligacao" class="btn btn-primary btn-lg fr" role="button">
+                        <a href="saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria" class="btn btn-primary btn-lg fr" role="button">
                             Leia mais 
                             <i class="fa fa-arrow-right"></i>
                         </a>
                         <div class="data-noticia">
-                            <i class="fa fa-calendar"></i> Publicado no dia 13/06/2018
+                            <i class="fa fa-calendar"></i> Publicado no dia 05/07/2018
                         </div>
                     </div>
                 </div>
                 <div class="mt-20"></div>
                 <div class="row">
-                    <div class="col-md-4">
-                        <a href="por-que-saber-o-minimo-de-logistica">
-                            <img src="img/por-que-saber-o-minimo-de-logistica-01.jpg" class="noticia-img">
+                    <div class="col-md-3">
+                        <a href="governanca-criativa-e-transporte-entenda-essa-ligacao">
+                            <img src="img/governanca-criativa-e-transporte-entenda-essa-ligacao.jpg" class="noticia-img mt-20">
                         </a>
                     </div>
-                    <div class="col-md-8">
+                    <div class="col-md-3">
                         <h3>
-                            POR QUE SABER O MÍNIMO DE LOGÍSTICA?
+                            Governança criativa e transporte: entenda essa ligação
                         </h3>
                         <p class="mb-20">
-                            Essa é uma bela pergunta para qualquer empresário. Você vende seus produtos, cota o frete e despacha para o cliente. E fim, certo? Na verdade, muitas vezes esse final não é tão feliz assim. A culpa...
+                            A sociedade moderna...
+                        </p>
+                        <a href="governanca-criativa-e-transporte-entenda-essa-ligacao" class="btn btn-primary fr" role="button">
+                            Leia mais 
+                            <i class="fa fa-arrow-right"></i>
+                        </a>
+                        <div class="data-noticia">
+                            <i class="fa fa-calendar"></i> Publicado no dia 01/05/2018
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <a href="por-que-saber-o-minimo-de-logistica">
+                            <img src="img/por-que-saber-o-minimo-de-logistica-01.jpg" class="noticia-img mt-20">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <h3>
+                            Por que saber o Mínimo de Logística?
+                        </h3>
+                        <p class="mb-20">
+                            Essa é uma bela pergunta para qualquer empresário. Você...
                         </p>
                         <a href="por-que-saber-o-minimo-de-logistica" class="btn btn-primary fr" role="button">
                             Leia mais 
---
+++ saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria.php
@@ -0,0 +1,71 @@
+	<?php $thisPage="Notícias"; ?>
+    <?php $your_meta_description="Descrição aqui"; ?>
+	<?php include("header.php"); ?>
+		<div class="page noticias">
+    		<div class="banner-page banner-noticias">
+    			<h1>NOTÍCIAS</h1>
+    		</div>
+    		<section>
+                <h2>
+                    Saiba quais são os diferentes tipos de carga rodoviária
+                </h2>
+                <div class="data-noticia interno mb-20">
+                    <i class="fa fa-calendar"></i> Publicado no dia 05/04/2018
+                </div>
+
+                <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-506da23b78eb0a51"></script>
+                <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                <div class="addthis_inline_share_toolbox"></div>
+            
+                <img src="img/saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria-01.jpg" class="noticia-interno-img">
+                <p>
+                    O crescimento e modernização da indústria ao redor do mundo, somado ao desenvolvimento tecnológico, que possibilita a conexão de pessoas e negócios ao redor do mundo, têm exigido que o setor de logística seja constantemente readequado para suprir a crescente demanda de transporte de carga.
+                </p>
+                <p>
+                    No Brasil, o principal modal de transporte, responsável por 76% da circulação de produtos e insumos, é o rodoviário, por alcançar, com facilidade, regiões onde diferentes opções, como aéreo ou aquaviário, não alcançam.
+                </p>
+                <p>
+                    Porém, apesar de ser a principal forma de transporte, será que é o mais adequado para todos os tipos de carga?
+                </p>
+                <p>
+                    Com certeza, não, uma vez que há fatores como distância, vida útil da carga – no caso de perecíveis – tamanho e peso, que fazem com que outros modais sejam mais adequados.
+                </p>
+                <p>
+                    Então vamos ver quais os principais tipos de carga são os mais adequados para o transporte rodoviário.
+                </p>
+                <h3>
+                    Principais tipos de carga para modal rodoviário
+                </h3>
+                <ul>
+                    <li>
+                        <strong>Cargas secas:</strong> São produtos não perecíveis, geralmente industrializados, que podem ser transportados em quaisquer horários e condições climáticas, sem que a carga seja danificada.Alguns dos principais tipos de cargas secas são:materiais de construção, equipamentos, madeiras, ferragens, produtos de limpeza, beleza, mobiliários, etc.
+                    </li>
+                    <li>
+                        <strong>Cargas perigosas:</strong> São todo e qualquer produto que apresente risco às pessoas, instalações e meio ambiente, sejam inflamáveis, explosivas, oxidantes, venenosas, infecciosas, poluentes, radioativas, ou corrosivas.Por isso, este tipo de carga deve ser transportado com uma série de cuidados, como identificação sobre a carga, empacotamento e acondicionamento, corretos, além de documentação correta e em dia.
+                    </li>
+                    <li>
+                        <strong>Cargas frigoríficas:</strong> Um dos tipos de carga que necessita de maior cuidado no transporte, inclusive o cumprimento, por parte das transportadoras, das normas e exigências para que estejam autorizadas a realizar este tipo de transporte.As cargas frigoríficas são, basicamente, compostas por itens de alimentação que devem ser acondicionados de forma a não sofrerem deterioração por temperatura, ou mesmo por falta de higiene dos caminhões.
+                    </li>
+                    <li>
+                        <strong>Cargas graneleiras:</strong> Este é o tipo de carga líquida ou sólida que não necessita ser acondicionada de forma específica, ou seja, não há necessidade de embalagens ou invólucros especiais.Apesar de não necessitarem ser acondicionadas de forma específica, devem ser transportadas individualmente, para que não se misturem e percam suas características originais, assim como não devem, em hipótese alguma, ser transportadas sem proteção.
+                    </li>
+                    <li>
+                        <strong>Cargas Vivas:</strong> Esta é a uma das modalidades mais delicadas de carga, pois consiste de animais vivos, necessitando de alto nível de especialização, uma vez que deve evitar o sofrimento dos animais.Este tipo de transporte deve ter espaço adequado para que o animal possa ficar de pé em posição natural, o caminhão deve ser livre de outros tipos de carga ou peças que possam machucar o animal, além de indicar que sua carga é viva e a posição em que se encontram.
+                    </li>
+                    <li>
+                        <strong>Carga veicular:</strong> Este tipo de carga é feito por caminhões cegonha ou guinchos e transportam, obviamente, veículos de todos os tipos e merecem um cuidado especial para que não se desprendam ou desbalaceiem o caminhão que os transporta, podendo causar sérios acidentes.
+                    </li>
+                    <li>
+                        <strong>Carga em pallets ou racks:</strong> Esta é uma modalidade de carga que pode levar quase qualquer produto, mas se destaca por facilitar a sustentação e acondicionamento de diferentes tipos de produtos, assim como seu manuseio na carga e descarga de caminhões que, geralmente, apresentam abertura lateral.
+                    </li>
+                </ul>
+                <p>
+                    Estes são os principais tipos de carga que circulam pelas estradas do Brasil e do mundo, com suas especificidades e cuidados necessários.
+                </p>
+                <p>
+                    Se você quer saber mais sobre fretamento e transporte de cargas, acesse o nosso blogpara conhecer soluções para a sua necessidade de transporte e entrega de produtos!
+                </p>
+    		</section>
+	    </div>
+	<?php include("footer.php"); ?>
