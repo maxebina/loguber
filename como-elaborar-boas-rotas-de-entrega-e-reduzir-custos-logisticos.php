39358fd093bdd7be892cdd7d9384ad90bce14852
Ebina <mxea@gft.com>
2018-08-15 14:47:48 -0300

    nova notícia

---
+++ como-elaborar-boas-rotas-de-entrega-e-reduzir-custos-logisticos.php
@@ -0,0 +1,109 @@
+	<?php $thisPage="Notícias"; ?>
+    <?php $your_meta_description="Descrição aqui"; ?>
+	<?php include("header.php"); ?>
+		<div class="page noticias">
+    		<div class="banner-page banner-noticias">
+    			<h1>NOTÍCIAS</h1>
+    		</div>
+    		<section>
+                <h2>
+                    Como elaborar boas rotas de entrega e reduzir custos logísticos?
+                </h2>
+                <div class="data-noticia interno mb-20">
+                    <i class="fa fa-calendar"></i> Publicado no dia 15/08/2018
+                </div>
+
+                <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-506da23b78eb0a51"></script>
+                <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                <div class="addthis_inline_share_toolbox"></div>
+            
+                <img src="img/como-elaborar-boas-rotas-de-entrega-e-reduzir-custos-logisticos.jpg" class="noticia-interno-img">
+                <p>
+                    Uma das formas de aumentar a lucratividade de uma empresa, é por meio da diminuição dos seus custos logísticos. E essa tarefa não é nada fácil! Uma maneira eficiente de começar é cortando despesas por meio de uma boa elaboração de rotas de entregas.
+                </p>
+                <p>
+                    Planejar as rotas de entregas a serem realizadas pela companhia em determinado período de tempo, permite aumentar a sua eficiência, minimizar os erros e torná-las viáveis financeiramente.
+                </p>
+                <p>
+                    No post de hoje daremos algumas dicas de como a sua empresa pode alcançar esse objetivo e assim obter o sucesso tão esperado. Ficou interessado no assunto? Então continue conosco e entenda melhor como desenvolver uma boa estratégia de rotas e reduzir os seus custos logísticos.
+                </p>
+                <h3>
+                    Como elaborar rotas de entregas de forma eficiente?
+                </h3>
+                <p>
+                    Confira abaixo algumas dicas para uma boa elaboração de rotas para a sua empresa:
+                </p>
+                <p>
+                    <strong>1. Escolha uma localização eficiente</strong>
+                </p>
+                <p>
+                    É fundamental escolher um local estratégico como um centro de distribuição para a sua empresa, pois é nesse momento que se inicia o planejamento de rotas. Uma localização ruim pode causar a elevação dos custos de distribuição, ao passo que uma escolha bem pensada pode otimizar o processo como um todo e aumentar os resultados da empresa.
+                </p>
+                <p>
+                    Por isso, é fundamental fazer uma busca correta ao procurar um local estratégico na distribuição logística da empresa, como galpões e centros de distribuição. Encontre um lugar que facilite o acesso à área de entrega com maior importância para o local.
+                </p>
+                <p>
+                    <strong>2. Utilize softwares para realizar planejamentos</strong>
+                </p>
+                <p>
+                    Você pode levar muito tempo para determinar uma boa rota utilizando mapas físicos ou arquivos do município do local de destino. E essa não é boa estratégia, pois principalmente para a logística, tempo é dinheiro.
+                </p>
+                <p>
+                    Atualmente, o mercado disponibiliza softwares de gestão — mais conhecidos como Roteirizadores de Entregas — capazes de realizar em segundos o que você gastaria horas para fazer, com uma margem de erro bem menor.
+                </p>
+                <p>
+                    Essa ferramenta tecnológica calcula, a partir dos pedidos pendentes, a melhor rota de entrega e aloca adequadamente a sua carga, baseando-se na frota disponível. Com a ajuda desse software você conseguirá:
+                </p>
+                <ul>
+                    <li>
+                        <strong>Reduzir os custos operacionais:</strong> pois será necessária uma equipe menor para o planejamento das entregas;
+                    </li>
+                    <li>
+                        <strong>Aumentar a sua eficiência:</strong> com a realização das entregas em menos tempo, o faturamento tende a ser maior;
+                    </li>
+                    <li>
+                        <strong>Agilizar a operação de roteirização e entrega;</strong>
+                    </li>
+                    <li>
+                        <strong>Alavancar a produtividade da equipe:</strong> já que diminuirá a necessidade de contratação de mão de obra ou uso de horas extras de motoristas e entregadores.
+                    </li>
+                </ul>
+                <p>
+                    <strong>3. Teste as rotas mais efetivas e econômicas</strong>
+                </p>
+                <ul>
+                    <li>
+                        O caminho mais rápido;
+                    </li>
+                    <li>
+                        A existência de pedágios;
+                    </li>
+                    <li>
+                        Desvios;
+                    </li>
+                    <li>
+                        Trânsito;
+                    </li>
+                    <li>
+                        Caminhos que levam ao menor consumo de combustível;
+                    </li>
+                    <li>
+                        Dentre outros.
+                    </li>
+                </ul>
+                <p>
+                    Estabelecendo a melhor rota, você economizará tempo e gastos com combustíveis e manutenção dos veículos.
+                </p>
+                <p>
+                    <strong>4. Use o tipo de veículo adequado</strong>
+                </p>
+                <p>
+                    O ideal para um bom planejamento de rotas, é que seja adotado um único veículo com capacidade para servir todos os destinos. Assim, alocar os veículos com maior disponibilidade de volume, é uma boa dica para diminuir as distâncias a serem percorridas e o prazo total para a realização das entregas.
+                </p>
+                <p>
+                    Viu só como uma boa elaboração de rotas de entregas na logística podem ajudar muito no crescimento de sua empresa? Agora que você já sabe como fazer, comece a colocar em prática todas essas dicas, para garantir a maior lucratividade com o seu negócio.
+                </p>
+    		</section>
+	    </div>
+	<?php include("footer.php"); ?>
--- noticias.php
+++ noticias.php
@@ -9,18 +9,42 @@
     		<section>
                 <div class="row">
                     <div class="col-md-6">
-                        <a href="saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria">
-                            <img src="img/saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria-01.jpg" class="noticia-img">
+                        <a href="como-elaborar-boas-rotas-de-entrega-e-reduzir-custos-logisticos">
+                            <img src="img/como-elaborar-boas-rotas-de-entrega-e-reduzir-custos-logisticos.jpg" class="noticia-img">
                         </a>
                     </div>
                     <div class="col-md-6">
                         <h2>
-                            Saiba quais são os diferentes tipos de carga rodoviária
+                            Como elaborar boas rotas de entrega e reduzir custos logísticos?
                         </h2>
                         <h3 class="mb-20">
-                            O crescimento e modernização da indústria ao redor do mundo, somado ao desenvolvimento tecnológico, que possibilita a...
+                            Uma das formas de aumentar a lucratividade de uma empresa, é por meio da diminuição dos seus custos logísticos. E essa tarefa não...
                         </h3>
-                        <a href="saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria" class="btn btn-primary btn-lg fr" role="button">
+                        <a href="como-elaborar-boas-rotas-de-entrega-e-reduzir-custos-logisticos" class="btn btn-primary btn-lg fr" role="button">
+                            Leia mais 
+                            <i class="fa fa-arrow-right"></i>
+                        </a>
+                        <div class="data-noticia">
+                            <i class="fa fa-calendar"></i> Publicado no dia 15/08/2018
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-20"></div>
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <a href="saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria">
+                            <img src="img/saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria-01.jpg" class="noticia-img mt-20">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <h3>
+                            Saiba quais são os diferentes tipos de carga rodoviária
+                        </h3>
+                        <p class="mb-20">
+                            O crescimento e modernização...
+                        </p>
+                        <a href="saiba-quais-sao-os-diferentes-tipos-de-carga-rodoviaria" class="btn btn-primary fr" role="button">
                             Leia mais 
                             <i class="fa fa-arrow-right"></i>
                         </a>
@@ -28,9 +52,7 @@
                             <i class="fa fa-calendar"></i> Publicado no dia 05/07/2018
                         </div>
                     </div>
-                </div>
-                <div class="mt-20"></div>
-                <div class="row">
+
                     <div class="col-md-3">
                         <a href="governanca-criativa-e-transporte-entenda-essa-ligacao">
                             <img src="img/governanca-criativa-e-transporte-entenda-essa-ligacao.jpg" class="noticia-img mt-20">
@@ -41,17 +63,20 @@
                             Governança criativa e transporte: entenda essa ligação
                         </h3>
                         <p class="mb-20">
-                            A sociedade moderna...
+                            A sociedade moderna conta não apenas com...
                         </p>
                         <a href="governanca-criativa-e-transporte-entenda-essa-ligacao" class="btn btn-primary fr" role="button">
                             Leia mais 
                             <i class="fa fa-arrow-right"></i>
                         </a>
                         <div class="data-noticia">
-                            <i class="fa fa-calendar"></i> Publicado no dia 01/05/2018
+                            <i class="fa fa-calendar"></i> Publicado no dia 13/06/2018
                         </div>
                     </div>
+                </div>
+                <div class="mt-20"></div>
 
+                <div class="row">
                     <div class="col-md-3">
                         <a href="por-que-saber-o-minimo-de-logistica">
                             <img src="img/por-que-saber-o-minimo-de-logistica-01.jpg" class="noticia-img mt-20">
@@ -73,6 +98,7 @@
                         </div>
                     </div>
                 </div>
+
     		</section>
 	    </div>
 	<?php include("footer.php"); ?>
